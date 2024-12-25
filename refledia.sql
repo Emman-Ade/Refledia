78b901fa1c8541efb81aa392569fcb32d592cd05
Emman-Ade <146605843+Emman-Ade@users.noreply.github.com>
2024-12-24 23:07:46 -0500

    Add files via upload

---
+++ refledia.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE entertainment (
+  entertainmentID INTEGER PRIMARY KEY,
+  rating INTEGER CHECK(rating BETWEEN 1 AND 10 ) ,
+  review text ,
+  isFinished ENUM('Finished','In Progress') NOT NULL
+  
+);
+CREATE TABLE user(
+userID INTEGER PRIMARY KEY,
+username VARCHAR(50) NOT NULL,
+password VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
+privlic ENUM ('public','private')  NOT NULL
+);
